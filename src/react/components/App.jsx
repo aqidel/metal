3c7092caa8ceaf584104d1d9f2d657316503115c
aqidel <fanofparadox@gmail.com>
2023-02-05 23:14:08 +0300

    main structure created

--- src/react/components/App.jsx
+++ src/react/components/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Ferrous from './pages/Ferrous';
+import Non_Ferrous from './pages/Non_Ferrous';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import '../../scss/style.scss';
 
 export default function App() {
   return (
     <React.Fragment>
       <Routes>
-        <Route path='/' element={<Main/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/ferrous' element={<Ferrous/>}/>
+        <Route path='/non-ferrous' element={<Non_Ferrous/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
       </Routes>
     </React.Fragment>
   );

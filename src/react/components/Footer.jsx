be82a31c3420e80a4c2986d70fd2a9690cb835c1
aqidel <fanofparadox@gmail.com>
2023-02-11 15:30:05 +0300

    footer, mobile header ready

--- src/react/components/Footer.jsx
+++ src/react/components/Footer.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <></>
+    <footer>
+      <p>
+        © ООО "БашМетЛом", 2022. Все права защищены. Информация на сайте не является публичной офертой.
+        <br/>
+        Developed by Aq-Idel
+      </p>
+    </footer>
   );
 }
--- src/react/components/Header.jsx
+++ src/react/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 import logo from '../../images/logo.png';
@@ -7,16 +7,33 @@ export default function Header() {
 
   const [state, setState] = useState(false);
 
+  const targetScroll = document.querySelector('body');
+
+  useEffect(() => {
+    state ? disableBodyScroll(targetScroll) : enableBodyScroll(targetScroll)
+  }, [state]);
+
   return (
     <header>
       <div id='header-logo-wrap'>
         <img src={logo} alt='logo'/>
-        <div id='burger-wrap'>
+        <div id='burger-wrap' onClick={() => setState(!state)}>
           <span className={'upper-line' + (state ? ' upper-line-active' : '')}/>
           <span className={'mid-line' + (state ? ' mid-line-active' : '')}/>
           <span className={'down-line' + (state ? ' down-line-active' : '')}/>
         </div>
       </div>
+      <nav id={state ? 'nav-open' : 'nav-closed'}>
+        <div/>
+        <Link to='/ferrous' onClick={() => setState(false)}>Черный лом</Link>
+        <div/>
+        <Link to='/non-ferrous' onClick={() => setState(false)}>Цветной лом</Link>
+        <div/>
+        <Link to='/about' onClick={() => setState(false)}>О нас</Link>
+        <div/>
+        <Link to='/contact' onClick={() => setState(false)}>Контакты</Link>
+        <div/>
+      </nav>
     </header>
   );
 }
